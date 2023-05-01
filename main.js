405126a711b407fde7e8813e3789a978b5b973cb
Mykhailo Zbihlei <mac@MacBook-Pro.local>
2023-05-01 12:58:37 +0300

    add visual / still not work good

--- main.js
+++ main.js
@@ -1,57 +1,97 @@
+const field = document.querySelector('.field__container');
 const singleField = document.querySelectorAll('.field__item');
- 
-// hasDuplicates = (arr)=> {
-//     let el =  new Set(arr).size; 
-//     if (el !== arr.length){
-//         console.log('found');
-//       }
-// }
-// double = (arr)=>{
-//     let doubleElemIndex;
-//     for (let i = 0; i < arr.length; i++) {
-//         for (let j = 0; j < i; j++)
-//         if (arr[j] === arr[i]) {
-//             doubleElemIndex = arr[j];
-//             return doubleElemIndex;
-//         }
-//     }
-// }
+const startbtn = document.querySelector('.startbtn');
+const playagain = document.querySelector('.playagain');
+const pressstart = document.querySelector('.pressstart');
+const youwin = document.querySelector('.youwin');
 
 
-showElement =()=>{
-    let elements = [];
+//timer 
+let timer = 0;
+let timerInterval;
+let ms = document.getElementById('ms');
+let second = document.getElementById('second');
+let minute = document.getElementById('minute');
 
+function start() {
+
+    pressstart.style.display = 'none';
+    field.classList.remove('pregame');
+
+  stop();
+  timerInterval = setInterval(function() {
+  timer += 1/60;
+  let msVal = Math.floor((timer - Math.floor(timer))*100);
+  let secondVal = Math.floor(timer) - Math.floor(timer/60) * 60;
+  let minuteVal = Math.floor(timer/60);
+  ms.innerHTML = msVal < 10 ? "0" + msVal.toString() : msVal;
+  second.innerHTML = secondVal < 10 ? "0" + secondVal.toString() : secondVal;
+  minute.innerHTML = minuteVal < 10 ? "0" + minuteVal.toString() : minuteVal;
+  }, 1000/60);
+}
+
+function stop() {
+  clearInterval(timerInterval);
+}
+
+
+let elements = [];
+let elementsCount = new Set();
+
+const showElement =()=>{
+   
     singleField.forEach.call(singleField,function(el){
+       
         el.addEventListener('click', ()=>{
 
                 el.classList.toggle('active');
                 setTimeout(()=>{
                     el.classList.remove('active');
                     elements.push(el.innerText);
+
                      for (let i =0; i<elements.length; i++){
                         if (elements[i] === elements[i-1]){
-                            el.classList.add('found');
-                            if (el.classList.contains('active found') ||el.classList.contains('active') ){
-                                el.classList.remove('active')
-                            }
-                            if (el.id !== el.innerText){
-                                el.classList.add('found');
-                            }
-                            document.getElementById(el.innerText).classList.add('found');
+                            // el.classList.add('found');
+                            // elements[i].classList.add('found');
+                            // elements[i-1].classList.add('found');
+                            // document.getElementById(elements[i]).classList.add('found');
+                            // document.getElementById(elements[i-1]).classList.add('found');
+
+                            // if (el.id !== el.innerText){
+                            //     el.classList.add('found');
+                            // }
+                            // document.getElementById(el.innerText).classList.add('found');
                            
                             elements = elements.splice();
                         }
                    }
-                },300);
-                
+                },300); 
           });
-          
     });
-    
-   
 }
 
-showElement();
+// if (/*smth*/){
+//     stop();
+//     field.classList.add('pregame');
+//     youwin.style.display = 'block';
+//     startbtn.style.display = 'none';
+//     playagain.style.display = 'block';
+// }
+
+
+startbtn.addEventListener('click', ()=>{
+    start();
+    showElement();
+});
+
+playagain.addEventListener('click', ()=>{
+    location.reload();
+});
+
+
+
+
+
 
 
 
